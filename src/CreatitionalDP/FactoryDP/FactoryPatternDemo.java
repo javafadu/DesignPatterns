ddd91c2b6c90767f6b61484986f888c819b1fe03
javafadu <javafadu@gmail.com>
2022-12-30 14:54:38 +0300

    30dec-v2

--- src/CreatitionalDP/FactoryDP/FactoryPatternDemo.java
+++ src/CreatitionalDP/FactoryDP/FactoryPatternDemo.java
@@ -2,6 +2,26 @@ package CreatitionalDP.FactoryDP;
 
 public class FactoryPatternDemo {
     public static void main(String[] args) {
+    /*
+        Circle circle1 = new Circle();
+        circle1.draw();
+
+     */
+        // We do not need to know domain class of to be created object?
+        // How can we create an object from Circle, Square or Rectangle
+
+        ShapeFactory shapeFactory = new ShapeFactory();
+        Shape shape1 = shapeFactory.getShape("Circle");
+        shape1.draw();
+
+        Shape shape2 = shapeFactory.getShape("square");
+        shape2.draw();
+
+        Shape shape3 = shapeFactory.getShape("Rectangle");
+        shape3.draw();
+
+
+
 
     }
 }
