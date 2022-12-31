f7e04f3d865f8e8dc4d4e541cfef1758d3dad3f3
javafadu <javafadu@gmail.com>
2023-01-01 00:04:46 +0300

    01Jan2023

--- src/CreationalDP/BuilderDP/BuilderDPRunner.java
+++ src/CreationalDP/BuilderDP/BuilderDPRunner.java
@@ -4,14 +4,28 @@ public class BuilderDPRunner {
     public static void main(String[] args) {
 
         // simple solution and not recommended
-        /*
-        Product product = new Product();
-        product.setId(1L);
-        product.setName("Computer");
-        product.setPrice(70);
-        */
+
+        Product product1 = new Product();
+        product1.setId(1L);
+        product1.setDescription("Casper");
+        product1.setName("Computer");
+        product1.setPrice(70);
 
 
+        // creating object wth BuilderDP
+        Product product2 = new Product.ProductBuilder()
+                .id(12L)
+                .name("Laptop")
+                .description("Macbook")
+                .price(100)
+                .build();
+
+
+        System.out.println("*********Product 1" );
+        System.out.println(product1);
+        System.out.println();
+        System.out.println("*********Product 2");
+        System.out.println(product2);
 
     }
 }
